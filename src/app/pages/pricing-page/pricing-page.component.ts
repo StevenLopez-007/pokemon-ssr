8ef8bbc2513d0ad6ca74b69a406438a7e3fbfd53
StevenLopez-007 <57334248+StevenLopez-007@users.noreply.github.com>
2024-09-17 20:53:18 -0600

    first commit

--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   standalone: true,
@@ -9,4 +10,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent { }
+export default class PricingPageComponent implements OnInit{
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    // if(isPlatformBrowser(this.platform)){
+    //   document.title = 'Pricing Page';
+    // }
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({name:'description',content:'Este es mi Pricing page'});
+    this.meta.updateTag({name:'og:title',content:'Pricing page'});
+    this.meta.updateTag({name:'keywords',content:'Hola, Mundo, Fernando'});
+  }
+
+}
